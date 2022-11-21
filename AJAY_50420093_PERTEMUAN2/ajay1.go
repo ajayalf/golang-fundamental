b7566735ed99fe07d20412b53a87a13402c4ae84
Ajay Alfredo Almani <49350537+ajayalf@users.noreply.github.com>
2022-11-21 22:22:34 +0700

    Golang Fundamental

---
+++ AJAY_50420093_PERTEMUAN2/ajay1.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main() {
+    fmt.Println("Ajay Alfredo Almani")
+    fmt.Println("50420093")
+}
---
+++ AJAY_50420093_PERTEMUAN2/ajay5.go
@@ -0,0 +1,7 @@
+package main 
+import "fmt"
+func main() {
+	var x string
+	x = "Ajay Alfredo"
+fmt.Println("My Name is", x)
+}
